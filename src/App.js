65be05f34f5cd559cc220314303acf059eab7558
arlestaboada <31904068+arlestaboada@users.noreply.github.com>
2022-05-08 21:23:07 -0500

    eliminar tarea
    eliminado ya

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React, {useState} from 'react'
-import { isEmpty } from 'lodash'
+import { isEmpty, size } from 'lodash'
 import shortid from 'shortid'
 
 function App() {
@@ -8,19 +8,27 @@ function App() {
   const [tasks, setTasks] = useState([])
   const addTask = (e) => {
 
-    e.preventDefault()
-    if(isEmpty(task)){
-      console.log("Task vacio")
-      return
-    }
-  const newTasks = {
-    id:shortid.generate,
-    name:task
+        e.preventDefault()
+        if(isEmpty(task)){
+          console.log("Task vacio")
+          return
+        }
+      const newTasks = {
+        id:shortid.generate(),
+        name:task
+      }
+      setTasks([...tasks, newTasks])
+      console.log("OK")
+      setTask("")
   }
-  setTasks([...tasks, newTasks])
-  console.log("OK")
-  setTask("")
+
+  const deleteTask = (id) => {
+    const filterTasks = tasks.filter(task => task.id !== id)
+    setTasks(filterTasks)
+
+
   }
+
 
   return (
     <div className="container mt-5">
@@ -29,22 +37,37 @@ function App() {
         <div className="row">
           <div className="col-8">
             <h4 className='text-center'>Lista de tareas</h4>
-            <ul className="list-group">
+            {
+              size(tasks)=== 0 ? 
+              (<h5 className='text-center'>No ahi tareas programadas</h5>):
+              (
+                <ul className="list-group">
 
-                  {
-                    tasks.map((task) => (
+                {
+                  tasks.map((task) => (
 
-                          <li className="list-group-item">
+                        <li className="list-group-item" key={task.id}>
                           <span className='lead'>{task.name}</span>
-                          <button className='btn btn-danger btn-sm float-right mx-2'>Editar</button>
-                          <button className='btn btn-warning btn-sm float-right'>Eliminar</button>
-                            </li>
-                        )
+                          <button className='btn btn-warning btn-sm float-right mx-2'>
+                            Editar
+                          </button>
+                          <button 
+                          className='btn btn-danger btn-sm float-right'
+                          onClick={()=>deleteTask(task.id)}>
+                            Eliminar
+                          </button>
+                         </li>
+                      )
 
-                    )
-                   
-                  }
-              </ul>
+                  )
+                 
+                }
+            </ul>
+
+
+              )
+            }
+           
           </div>
           <div className="col-4">
               <h4 className='text-center'>Formulario</h4>
